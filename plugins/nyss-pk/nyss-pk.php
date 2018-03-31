6bef7063cc2e3d5b42d6cdd745ae1c8e5c2b69bb
Mervin Hernandez <me@mervinhernandez.com>
2018-03-30 22:58:39 -0400

    PIKLIST - created custom plugin

---
+++ plugins/nyss-pk/nyss-pk.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: NYSS Customizations
+* Plugin URI: http://nysoccer-skills.com
+* Description: Custom WordPress magic for NY Soccer Skills
+* Version: 0.0.1
+* Author: Mervin Hernandez
+* Author URI: http://mervinhernandez.com
+* Plugin Type: Piklist
+*/
